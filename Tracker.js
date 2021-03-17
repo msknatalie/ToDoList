b1b51a0cc310c62ea72164254c6751dd8bc91868
msknatalie <cgs.korneeva@gmail.com>
2021-03-17 22:01:27 +0300

    Working on sorting

--- Tracker.js
+++ Tracker.js
@@ -1,84 +1,68 @@
 let taskContainer = document.querySelector("#taskContainer");
 
-function download () {
-    let newEachTaskContainer = document.createElement('div');
-    newEachTaskContainer.classList.add('eachTaskContainer'); 
-    let input = document.createElement('input');
-        input.classList.add('task');
-        newEachTaskContainer.append(input);
-        let butDelete = document.createElement('img');
-        butDelete.src = "img/cross.png";
-        butDelete.classList.add("delete");
-        newEachTaskContainer.append(butDelete);
+function download() {
+  let newEachTaskContainer = document.createElement("div");
+  newEachTaskContainer.classList.add("eachTaskContainer");
+  let input = document.createElement("input");
+  input.classList.add("task");
+  newEachTaskContainer.append(input);
+  let butDelete = document.createElement("img");
+  butDelete.src = "img/cross.png";
+  butDelete.classList.add("delete");
+  newEachTaskContainer.append(butDelete);
 
-        taskContainer.append(newEachTaskContainer);
+  taskContainer.append(newEachTaskContainer);
 
-        butDelete.addEventListener("click", () => {
-            input.remove();
-            butDelete.remove();
-        })
+  butDelete.addEventListener("click", () => {
+    input.remove();
+    butDelete.remove();
+  });
 }
 
 download();
 
 let add = document.querySelector("#add");
 
-add.addEventListener("click", download => {
-    let newEachTaskContainer = document.createElement('div');
-    newEachTaskContainer.classList.add('eachTaskContainer'); 
-    let input = document.createElement('input');
-        input.classList.add('task');
-        newEachTaskContainer.append(input);
-        let butDelete = document.createElement('img');
-        butDelete.src = "img/cross.png";
-        butDelete.classList.add("delete");
-        newEachTaskContainer.append(butDelete);
-
-        taskContainer.append(newEachTaskContainer);
-
-        butDelete.addEventListener("click", () => {
-            input.remove();
-            butDelete.remove();
-        })
-    })
+add.addEventListener("click", download);
 
 // sorting
-let sortingButton = document.querySelector('#sorting');
+let sortingButton = document.querySelector("#sorting");
 let pointer = true;
 
 sortingButton.addEventListener("click", () => {
-    let tasks = document.querySelectorAll(".task");
-    let taskArray = [...tasks];
-    if (pointer === true) {
+  let tasks = document.querySelectorAll(".eachTaskContainer");
+  let taskArray = [...tasks];
+  
+  if (pointer === true) {
     taskArray.sort((first, second) => {
-        let pam1 = first.querySelector('.task').value;
-        let pam2 = second.querySelector('.task').value;
-        if (pam1 > pam2) {
-            return 1;
-        } else if (pam1 < pam2) {
-            return -1;
-        } else {
-            return 0;
-        }
-    })
+      let pam1 = first.querySelector("input").value;
+      let pam2 = second.querySelector("input").value;
+      if (pam1 > pam2) {
+        return 1;
+      } else if (pam1 < pam2) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
     sortingButton.src = "img/up.png";
     pointer = false;
-    } else if (pointer === false) {
+  } else if (pointer === false) {
     tasksArray.sort((first, second) => {
-        let pam1 = first.querySelector('.task').value;
-        let pam2 = second.querySelector('.task').value;
-        if (pam1 > pam2) {
-            return - 1;
-        } else if (pam1 < pam2) {
-            return 1;
-        } else {
-            return 0;
-        }
-    })
+      let pam1 = first.querySelector("input").value;
+      let pam2 = second.querySelector("input").value;
+      if (pam1 > pam2) {
+        return -1;
+      } else if (pam1 < pam2) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
     sortingButton.src = "img/down.png";
     pointer = true;
-    }
-    tasksArray.forEach(curName => {
-        taskContainer.append(curName);
-    });
-})
+  }
+  tasksArray.forEach((curName) => {
+    taskContainer.append(curName);
+  });
+});
